b4d62338e313302b4c50ad9fb6f7050fb774aac7
DonnaLi <lidonna318@gmail.com>
2025-12-20 14:39:17 -0800

    Initial Commit

---
+++ porsche directory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  porsche directory
+//
+//  Created by Donna Li on 2025-12-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ porsche directory/porsche_directoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  porsche_directoryApp.swift
+//  porsche directory
+//
+//  Created by Donna Li on 2025-12-20.
+//
+
+import SwiftUI
+
+@main
+struct porsche_directoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
